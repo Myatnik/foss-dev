807ba771efe8af3aa3e0298cfa71962a01569905
Maksim Shvetsov <maxik_shvetsov@mail.ru>
2026-03-04 16:13:13 +0800

    Added another function and test for it

--- testing/tests/test_principles.py
+++ testing/tests/test_principles.py
@@ -3,11 +3,19 @@ sys.path.append(".../src")
 
 #TODO make it wit pip install
 
-from math_demo import add
+from math_demo import (add, add_with_bug)
 
 def test_addition():
     assert add(2, 2) == 4;
-    print("Test BASIC ADDITION")
+    print("Test BASIC ADDITION PASSED")
+#
+def test_addition_bugged():
+    assert add_with_bug(2, 2) == 4;
+    assert add_with_bug(0, 0) == 0;
+    print("Test BUGGED ADDITION PASSED")
+#
+
 
 if __name__ == "__main__":
     test_addition()
+    test_addition_bugged()
