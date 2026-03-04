996d74c582101597200152a1c9324995d302cf5b
Maksim Shvetsov <maxik_shvetsov@mail.ru>
2026-03-04 16:20:24 +0800

    Added third test, tests should not test logic

--- testing/tests/test_principles.py
+++ testing/tests/test_principles.py
@@ -13,9 +13,13 @@ def test_addition_bugged():
     assert add_with_bug(2, 2) == 4;
     assert add_with_bug(0, 0) == 0;
     print("Test BUGGED ADDITION PASSED")
+    assert add_with_bug(3,6) == 13;
 #
+def test_addition_duplicated():
+    assert add(2, 3) == 2+3
 
 
 if __name__ == "__main__":
     test_addition()
     test_addition_bugged()
+    test_addition_duplicated()
