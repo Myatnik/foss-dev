f720b748502ee9d7a70d2dfe16b150bb9e8d05d6
Maksim Shvetsov <maxik_shvetsov@mail.ru>
2026-06-07 16:15:48 +0800

    changed setup.py, hopefully everything ready for test/pypi

--- testing_tdd/setup.py
+++ testing_tdd/setup.py
@@ -2,7 +2,7 @@ from setuptools import (setup, find_packages)
 
 setup(
 	name = "ndfl_myatnik",
-	version = "0.0.0",
+	version = "0.0.1",
 	package_dir = {"": "src"},
 	packages = find_packages(where="src"),
 	long_description = "Tax calculator",
