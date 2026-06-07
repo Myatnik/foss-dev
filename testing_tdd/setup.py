8500e970b373a93b2c0ab8da5b121d2d202e7722
Maksim Shvetsov <maxik_shvetsov@mail.ru>
2026-06-07 16:50:07 +0800

    added repository link to the description

--- testing_tdd/setup.py
+++ testing_tdd/setup.py
@@ -2,9 +2,9 @@ from setuptools import (setup, find_packages)
 
 setup(
 	name = "ndfl_myatnik",
-	version = "0.0.1",
+	version = "0.0.2",
 	package_dir = {"": "src"},
 	packages = find_packages(where="src"),
-	long_description = "Tax calculator",
+	long_description = "Tax calculator https://github.com/Myatnik/foss-dev",
 	long_description_content_type = "text/markdown"
 )
